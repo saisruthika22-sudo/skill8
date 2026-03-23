909034296f28f894545c9f25e933363d8f094580
saisruthika22-sudo <saisruthika22@gmail.com>
2026-03-23 19:09:23 +0530

    Update ProductController.java

--- src/main/java/com/klu/skill8/ProductController.java
+++ src/main/java/com/klu/skill8/ProductController.java
@@ -1,5 +1,41 @@
 package com.klu.skill8;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 
+    private final ProductRepository repo;
+
+    public ProductController(ProductRepository repo) {
+        this.repo = repo;
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return repo.save(product);
+    }
+
+    @GetMapping("/category/{category}")
+    public List<Product> getByCategory(@PathVariable String category) {
+        return repo.findByCategory(category);
+    }
+
+    @GetMapping("/filter")
+    public List<Product> filterByPrice(@RequestParam double min,
+                                       @RequestParam double max) {
+        return repo.findByPriceBetween(min, max);
+    }
+
+    @GetMapping("/sorted")
+    public List<Product> sortByPrice() {
+        return repo.sortByPrice();
+    }
+
+    @GetMapping("/expensive/{price}")
+    public List<Product> expensive(@PathVariable double price) {
+        return repo.findExpensiveProducts(price);
+    }
 }
