c2e1e8453fb21af01beea42a0897a9dc82e5744e
saisruthika22-sudo <saisruthika22@gmail.com>
2026-03-23 19:12:53 +0530

    Update product.java

--- src/main/java/com/klu/skill8/product.java
+++ src/main/java/com/klu/skill8/product.java
@@ -1,5 +1,28 @@
 package com.klu.skill8;
 
-public class product {
+import jakarta.persistence.*;
 
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private double price;
+
+    public Product() {}
+
+    public Long getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
 }
